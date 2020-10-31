50268792e9f1602c093e6c0f4d12b420c5167784
bocahrokok <indieunistudy@gmail.com>
2020-10-31 19:26:45 +0700

    marshaling data

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/algolia/algoliasearch-client-go/v3/algolia/opt"
 	"github.com/gin-gonic/gin"
-
 	"github.com/nurrizkyimani/fabelio/database"
+	"github.com/nurrizkyimani/fabelio/model"
 )	
 
 func main(){
@@ -14,9 +15,6 @@ func main(){
 
 	index := database.InitAlgolia()
 
-	
-
-	
 	
 
 	fmt.Printf("print")
@@ -30,26 +28,62 @@ func main(){
 		// _ := c.Request.Body
 
 
+		// jsonData, err := ioutil.ReadAll(c.Request.Body)
+		// if err != nil {
+		// 		// Handle error
+		// }
+
+
+
 		
 		params := []interface{} {
 			opt.AttributesToRetrieve("ProductName", "Colours"),	
 		}
 
 		res , err := index.Search(keyword, params...)
+		reshit := res.Hits
 
 		if err != nil {
 			panic("panic on indexing in get ")
 		}
 
+		
+		b, err := json.Marshal(reshit)
 
-		c.JSON(200, res)
+		var a[] model.Hit
+		err = json.Unmarshal(b, &a)
+
+		if err != nil {
+				fmt.Println("error:", err)
+		}
+
+		o := a[0].ObjectID
+
+		newUpdate := model.ProductSeenUpdate{
+			ObjectID: o ,
+			Seen: true,
+		}	
+
+		res1, err := index.SaveObject(newUpdate)
+
+		if err != nil {
+				fmt.Println("error:", err)
+		}
+		
+		fmt.Println(res1)
+		
+		c.JSON(200, a[0] )
 
 	})
+
+
+
 	r.GET("/test", func( c *gin.Context){
 		c.JSON(200, gin.H{
 			"message": "Hello world",
 		})
 	})
+
 
 
 
--- model/model.go
+++ model/model.go
@@ -1,12 +1,30 @@
 package model
 
-type product struct {
+type Product struct {
  ProductName string `json:"ProductName"`
- Available string `json:"Available"`
- Seen string `json:"Seen"`
+ Available bool `json:"Available"`
+ Seen bool `json:"Seen"`
  Price int `json:"Price"`
  Dimension string `json:"Dimension"`
  Colours []string `json:"Colours"`
  Material string `json:"Material"`
  Image string `json:"Image"`
+}
+
+
+type ProductSeenUpdate struct  {
+	ObjectID string `json:"ObjectID"`
+	Seen bool`json:"Seen"`
+}
+
+//Response is a xxxx
+type Respoonse struct {
+	Params string `json:"Params"`
+	Hits []Hit `json:"Hits"`
+}
+
+
+type Hit struct {
+	ProductName string `json:"ProductName"`
+	ObjectID string `json:"ObjectID"`
 }
